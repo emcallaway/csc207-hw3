31601b92aaa65af1e13b5ccea17ead58c99689a1
emcallaway <callaway@grinnell.edu>
2014-02-01 16:33:52 -0600

    splitAt method created

--- src/edu/grinnell/csc207/callaway/utils/StringUtils.java
+++ src/edu/grinnell/csc207/callaway/utils/StringUtils.java
@@ -1,5 +1,39 @@
 package edu.grinnell.csc207.callaway.utils;
+import java.util.*;
 
 public class StringUtils {
-
+    public static String[] splitAt (String str, char seperator)
+    {
+	int size=1;
+    
+	for(int i=0; i<str.length(); i++)
+	    {
+		if (str.charAt(i) == seperator)
+		{ size++;}}
+	String newArray[] = new String[size];
+	int k=0;
+	int count=0;
+	    for(int i=0; i<str.length(); i++)
+	    {
+		if (str.charAt(i) == seperator)
+		{
+		    if (k>0)
+			count--;
+		    newArray[k] = str.substring(i-count, i);
+		    k++;
+		    count=0;	
+		}
+		else if (i == str.length()-1)
+		{
+		    newArray[k] = str.substring(i-count+1);
+		}
+		count++;
+	    }
+	return newArray;
+    }   
+    
+    public static void main(String[] args)
+    {
+	System.out.println(Arrays.toString(splitAt("as34:bzd:cdsf:sdf", ':')));
+    }
 }

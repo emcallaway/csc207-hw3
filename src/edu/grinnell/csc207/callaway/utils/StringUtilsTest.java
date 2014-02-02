6192809209f5bf108bbe8705ec670b003c76b1e4
emcallaway <callaway@grinnell.edu>
2014-02-01 23:17:49 -0600

    Attempt at remaking splitCSV method.... added test for it

--- src/edu/grinnell/csc207/callaway/utils/StringUtilsTest.java
+++ src/edu/grinnell/csc207/callaway/utils/StringUtilsTest.java
@@ -23,5 +23,15 @@ public class StringUtilsTest {
 	assertArrayEquals("trailing empty field", new String[] { "a", "" },
 		StringUtils.splitAt("a:", ':'));
     }
+    
+    @Test
+    public void testSplitCSV() {
+	assertArrayEquals (new String[] { "a", "b", "c" },
+                StringUtils.splitCSV("a,b,c"));
+//	assertArrayEquals (new String[] { "a,b", "c" },
+//                StringUtils.splitCSV("\"a,b\",c"));
+	assertArrayEquals (new String[] { "a", "b,b\"", "c" },
+                StringUtils.splitCSV("a,\"b,b\"\"\",c"));
+    }
 
 }

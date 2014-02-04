397dd971c5dc6d0ca6a1375c608f54a4e1799808
emcallaway <callaway@grinnell.edu>
2014-02-03 21:46:45 -0600

    eval0 method and tester

--- src/edu/grinnell/csc207/callaway/utils/Calculator.java
+++ src/edu/grinnell/csc207/callaway/utils/Calculator.java
@@ -1,5 +1,65 @@
 package edu.grinnell.csc207.callaway.utils;
 
+import java.math.BigInteger;
+
 public class Calculator {
 
-}
+    public static BigInteger eval0(String str)
+    {
+	int j=0, k=0;
+	while (j<(str.length()) && str.charAt(j)!= ' ')
+	    j++;
+	BigInteger num = BigInteger.valueOf(Long.parseLong(str.substring(0, j)));
+	
+	for(int i=0; i<str.length(); i++)
+	{
+	    if (str.charAt(i)== ' ')
+	    {
+		i++;
+		k=i+2;
+		if (str.charAt(i)=='+')
+		{
+    			while (k<(str.length()) && !(str.charAt(k)==' '))
+    			    k++;
+    			num= num.add(BigInteger.valueOf(Long.parseLong(str.substring(i+2, k))));
+    			k=0;
+		}//if(+)
+		else if (str.charAt(i)=='-')
+		{
+		    while (k<(str.length()) && !(str.charAt(k)==' '))
+			    k++;
+			num= num.subtract(BigInteger.valueOf(Long.parseLong(str.substring(i+2, k))));
+			k=0;
+		}//if(-)
+		else if (str.charAt(i)=='*')
+		{
+		    while (k<(str.length()) && !(str.charAt(k)==' '))
+			    k++;
+			num= num.multiply(BigInteger.valueOf(Long.parseLong(str.substring(i+2, k))));
+			k=0;
+		}//if(*)
+		else if (str.charAt(i)=='/')
+		{
+		    while (k<(str.length()) && !(str.charAt(k)==' '))
+			    k++;
+			num= num.divide(BigInteger.valueOf(Long.parseLong(str.substring(i+2, k))));
+			k=0;
+		}//if(/)
+		else if (str.charAt(i)=='^')
+		{
+		    while (k<(str.length()) && !(str.charAt(k)==' '))
+			    k++;
+			num= num.pow(Integer.parseInt(str.substring(i+2, k)));//(Long.parseLong(str.substring(i+2, k))));
+			k=0;
+		}//if(^)
+		
+	    }//if(' ')
+	}//for
+	return num;
+    }//eval0
+    
+    public static void main(String[]args)
+    {
+	System.out.println(eval0("44 - 23 + 434 * 2 / 4 ^ 5"));
+    }
+}//Calculator
--- src/edu/grinnell/csc207/callaway/utils/StringUtilsTest.java
+++ src/edu/grinnell/csc207/callaway/utils/StringUtilsTest.java
@@ -2,12 +2,14 @@ package edu.grinnell.csc207.callaway.utils;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 public class StringUtilsTest {
 
     @Test
-    public void testSplitAt() {
+    public void splitAtTest() {
 	assertArrayEquals("1", new String[] { "a", "b", "c" },
 		StringUtils.splitAt("a:b:c", ':'));
 	assertArrayEquals("2",new String[] { "a", "b", "c" },
@@ -22,7 +24,7 @@ public class StringUtilsTest {
 		StringUtils.splitAt(":a", ':'));
 	assertArrayEquals("trailing empty field", new String[] { "a", "" },
 		StringUtils.splitAt("a:", ':'));
-    }
+    }//splitAtTest
   /*  
     @Test
     public void testSplitCSV() {
@@ -35,11 +37,24 @@ public class StringUtilsTest {
     }*/
     
     @Test
-    public void testdeLeet() {
+    public void deLeetTest() {
     	assertEquals ("e", StringUtils.deLeet ("3"));
     	assertEquals ("leet", StringUtils.deLeet ("133+"));
     	assertEquals ("eat banana", StringUtils.deLeet ("3@+ |3@|\\|@|\\|@"));
     	assertEquals ("eat dd |xeacnan", StringUtils.deLeet ("3@+ dd |x3@c|\\|@|\\|"));
-    }
+    }//deLeetTest
+    
+    @Test
+    public void eval0Test() {
+	assertEquals (BigInteger.valueOf (0), Calculator.eval0 ("0"));
+	assertEquals (BigInteger.valueOf (2), Calculator.eval0 ("1 + 1"));
+	assertEquals (BigInteger.valueOf (4), Calculator.eval0 ("1 + 2 + 1"));
+	assertEquals (BigInteger.valueOf (9), Calculator.eval0 ("1 + 2 * 3"));
+	assertEquals (BigInteger.valueOf (1), Calculator.eval0 ("1 - 2 + 4 / 3"));
+	assertEquals (BigInteger.valueOf (-2), Calculator.eval0 ("1 - 5 / 2"));
+	assertEquals (BigInteger.valueOf (27), Calculator.eval0 ("1 + 2 ^ 3"));
+	
+    }//eval0Test
+    
 
 }
